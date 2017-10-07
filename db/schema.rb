2db356615bca6a6419a03a81666420f0f561875e
Mathieu Audebert <matheosmat@gmail.com>
2017-10-07 14:55:47 +0200

    add address at flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005105744) do
+ActiveRecord::Schema.define(version: 20171007124507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171005105744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "background_url"
+    t.string "address"
   end
 
 end
