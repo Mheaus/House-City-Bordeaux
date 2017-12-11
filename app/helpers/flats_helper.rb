09eb068467c8fefb37d7ec87428e6e0ceeaaa7d6
Mathieu Audebert <mheos@macbook-pro-de-mathieu.home>
2017-12-11 13:48:06 +0100

    complete more the flats#index and add new icons

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -1,7 +1,3 @@
 module FlatsHelper
-  def show_svg(path)
-    File.open("app/assets/images/icons/#{path}", "rb") do |file|
-      raw file.read
-    end
-  end
+  # http://cobwwweb.com/render-inline-svg-rails-middleman
 end
