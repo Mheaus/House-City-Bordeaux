e5443149817a40a61f9403ebff89263d29dcb304
Mathieu Audebert <mheos@macbook-pro-de-mathieu.home>
2017-12-18 21:35:14 +0100

    new design for flats index and his panel

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -1,3 +1,8 @@
 module FlatsHelper
   # http://cobwwweb.com/render-inline-svg-rails-middleman
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/icons/#{name}"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
 end
